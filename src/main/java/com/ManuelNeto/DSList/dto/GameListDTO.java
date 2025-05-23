4486c088f230de2b646798b8369b3a2a62bd572e
NoClick11 <bahamonde.boullosa@gmail.com>
2025-05-22 21:45:39 -0300

    Get lists

---
+++ src/main/java/com/ManuelNeto/DSList/dto/GameListDTO.java
@@ -0,0 +1,27 @@
+package com.ManuelNeto.DSList.dto;
+
+import com.ManuelNeto.DSList.entities.GameList;
+
+public class GameListDTO {
+
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+	}
+
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+		
+	
+}
